dcb2b0e431959c2b8d0555aeb63066f86ef0a8b6
Guadalupe1229 <145031760+Guadalupe1229@users.noreply.github.com>
2025-10-17 12:13:51 -0600

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,16 +2,9 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // mysql.railway.internal
-  user: process.env.DB_USER,       // root
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,   // railway
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10
-});
+// Crear pool usando la URL completa de Railway
+const pool = mysql.createPool(process.env.DB_URL);
 
+console.log('DEBUG typeof pool:', typeof pool); // debe mostrar "object"
 
-console.log('DEBUG typeof pool:', typeof pool);
 module.exports = pool;
