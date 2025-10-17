3dcd421cefec8ff615cacf6ddc693cbb2270c629
Guadalupe1229 <145031760+Guadalupe1229@users.noreply.github.com>
2025-10-17 11:46:38 -0600

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,11 +5,12 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123',
-  database: process.env.DB_NAME || 'justificantes_db',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'railway',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10
 });
+
 console.log('DEBUG typeof pool:', typeof pool);
 module.exports = pool;
